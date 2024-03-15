3020e23f1fb5d4cb8a535953c090cbef8ac72e7f
DanielNuud <daaniel.nuud@gmail.com>
2024-03-15 15:03:37 +0200

    fixing checkstyle errors

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -13,14 +13,14 @@ import java.util.Locale;
 @EnableJpaAuditing
 @EnableConfigurationProperties
 public class AppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AppApplication.class, args);
+  }
 
-	@Bean
-	public Faker getFaker() {
-		return new Faker(new Locale("en", "US"));
-	}
+  @Bean
+  public Faker getFaker() {
+    return new Faker(new Locale("en", "US"));
+  }
 
 
 }
