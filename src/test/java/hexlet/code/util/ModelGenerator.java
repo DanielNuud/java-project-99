a63f68fc49d17c5fa6c4b2f42e26305e2f55b344
DanielNuud <daaniel.nuud@gmail.com>
2024-03-23 14:38:53 +0200

    create TaskControllerTest

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -1,5 +1,7 @@
 package hexlet.code.util;
 
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import jakarta.annotation.PostConstruct;
@@ -18,6 +20,9 @@ public class ModelGenerator {
     private Model<User> userModel;
 
     private Model<TaskStatus> statusModel;
+    private Model<Task> taskModel;
+    private Model<Label> labelModel;
+
 
     @Autowired
     private Faker faker;
@@ -36,6 +41,22 @@ public class ModelGenerator {
                 .ignore(Select.field("tasks"))
                 .supply(Select.field("name"), () -> faker.animal().name())
                 .supply(Select.field("slug"), () -> faker.lorem().word())
+                .toModel();
+
+        taskModel = Instancio.of(Task.class)
+                .ignore(Select.field("id"))
+                .ignore(Select.field("taskStatus"))
+                .ignore(Select.field("assignee"))
+                .ignore(Select.field("labels"))
+                .supply(Select.field("name"), () -> faker.beer().name())
+                .supply(Select.field("index"), () -> faker.number().positive())
+                .supply(Select.field("description"), () -> faker.beer().brand())
+                .toModel();
+
+        labelModel = Instancio.of(Label.class)
+                .ignore(Select.field("id"))
+                .ignore(Select.field("tasks"))
+                .supply(Select.field("name"), () -> faker.lorem().characters(3, 1000))
                 .toModel();
     }
 }
