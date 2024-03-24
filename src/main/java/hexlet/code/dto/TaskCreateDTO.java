5d652c03f52046ced1651d3f4ff53a14d2c98bc5
DanielNuud <daaniel.nuud@gmail.com>
2024-03-24 21:30:49 +0200

    checkstyle fixes

--- src/main/java/hexlet/code/dto/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/TaskCreateDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
--- src/main/java/hexlet/code/dto/TaskDTO.java
+++ src/main/java/hexlet/code/dto/TaskDTO.java
@@ -1,6 +1,5 @@
 package hexlet.code.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
