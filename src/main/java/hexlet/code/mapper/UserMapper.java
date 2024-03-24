111673933bf3f6a90ababc3479b05cf013f6259d
DanielNuud <daaniel.nuud@gmail.com>
2024-03-24 21:46:31 +0200

    update UserMapper

--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -4,12 +4,7 @@ import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.model.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.BeforeMapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,11 +22,11 @@ public abstract class UserMapper {
         data.setPassword(passwordEncoder.encode(password));
     }
 
-
+    @Mapping(target = "passwordDigest", source = "password")
     public abstract User map(UserCreateDTO dto);
-
 
     public abstract UserDTO map(User model);
 
+    @Mapping(target = "passwordDigest", ignore = true)
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
 }
